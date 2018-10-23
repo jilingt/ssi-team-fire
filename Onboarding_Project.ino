932499c3bee55a3a6296c641391a715e4a83e716
jilingt <44306377+jilingt@users.noreply.github.com>
2018-10-22 22:44:39 -0700

    I2C for BMP
    Finally works! Prints BMP data to SD card.

--- Onboarding_Project.ino
+++ Onboarding_Project.ino
@@ -1,6 +1,6 @@
 /* Onboarding Project - Team Fire
  * 
- * Writes data but data from BMP is inaccurate
+ * Updated to initialize and de-initialize each sensor
  */
 
 #include <Wire.h>
@@ -15,11 +15,13 @@ File myFile;
 
 const int SD_CS = 9;
 const int BMP_CS = 10;
-const double altCalibrate = 1013.25;
+const double altCalibrate = 1014.5;
 
-Adafruit_BMP280 bme(BMP_CS);
+int times = 0;
 
-void writeToFile(char filename[], char writeLine[]) {
+Adafruit_BMP280 bme; // (BMP_CS);
+
+void writeToFile(char filename[], float writeLine) {
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
   myFile = sd.open(filename, FILE_WRITE);
@@ -39,86 +41,8 @@ void writeToFile(char filename[], char writeLine[]) {
   }
 }
 
-//void writeToFile(String filename, String writeLine) {
-//  // open the file. note that only one file can be open at a time,
-//  // so you have to close this one before opening another.
-//  myFile = sd.open(filename, FILE_WRITE);
-//
-//  // if the file opened okay, write to it:
-//  if (myFile) {
-//    Serial.print("Writing to " + filename);
-//    myFile.print(writeLine);
-//    // close the file:
-//    myFile.close();
-//    Serial.println(". Done.");
-//  } else {
-//    // if the file didn't open, print an error:
-//    Serial.println("error opening " + filename);
-//  }
-//}
-
-//void writeTemperature(String filename) {
-//  // open the file. note that only one file can be open at a time,
-//  // so you have to close this one before opening another.
-//  myFile = sd.open(filename, FILE_WRITE);
-//
-//  // if the file opened okay, write to it:
-//  if (myFile) {
-//    Serial.print("Writing to " + filename);
-//    myFile.print("Temperature = ");
-//    myFile.print(bme.readTemperature());
-//    myFile.println(" *C");
-//    // close the file:
-//    myFile.close();
-//    Serial.println(". Done.");
-//  } else {
-//    // if the file didn't open, print an error:
-//    Serial.println("error opening " + filename);
-//  }
-//}
-//
-//void writePressure(String filename) {
-//  // open the file. note that only one file can be open at a time,
-//  // so you have to close this one before opening another.
-//  myFile = sd.open(filename, FILE_WRITE);
-//
-//  // if the file opened okay, write to it:
-//  if (myFile) {
-//    Serial.print("Writing to " + filename);
-//    myFile.print("Pressure = ");
-//    myFile.print(bme.readPressure());
-//    myFile.println(" Pa");
-//    // close the file:
-//    myFile.close();
-//    Serial.println(". Done.");
-//  } else {
-//    // if the file didn't open, print an error:
-//    Serial.println("error opening " + filename);
-//  }
-//}
-
-//void writeAltitude(String filename) {
-//  // open the file. note that only one file can be open at a time,
-//  // so you have to close this one before opening another.
-//  myFile = sd.open(filename, FILE_WRITE);
-//
-//  // if the file opened okay, write to it:
-//  if (myFile) {
-//    Serial.print("Writing to " + filename);
-//    myFile.print("Approx altitude = ");
-//    myFile.print(bme.readAltitude(altCalibrate));
-//    myFile.println(" Pa");
-//    // close the file:
-//    myFile.close();
-//    Serial.println(". Done.");
-//  } else {
-//    // if the file didn't open, print an error:
-//    Serial.println("error opening " + filename);
-//  }
-//}
-
 void readFile(char filename[]) {
-  // re-open the file for reading:
+  // open the file for reading:
   myFile = sd.open(filename);
   if (myFile) {
     Serial.print(filename);
@@ -139,24 +63,34 @@ void readFile(char filename[]) {
   }
 }
 
+float readBMP(char readWhat[]) {
+  if(strcmp(readWhat, "temp") == 0) {
+    return bme.readTemperature();
+  } else if(strcmp(readWhat, "pres") == 0) {
+    return bme.readPressure();
+  } else if(strcmp(readWhat, "alti") == 0) {
+    return bme.readAltitude();
+  } else {
+    Serial.println("parameter not found!");
+  }
+
+  return 0;
+}
+
+
+
 void setup() {
-  SPI.setMOSI(11);
-  SPI.setMISO(12);
-  SPI.setSCK(13);
-  
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-  
-  if (!bme.begin()) {  
-    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
-    while (1);
-  } else {
-    Serial.println("BMP280 sensor detected.");
-  }
 
+  SPI.setMOSI(11);
+  SPI.setMISO(12);
+  SPI.setSCK(13);
+  
+  // Initialize SD card
   Serial.print("Initializing SD card...");
   if (!sd.begin(SD_CS, SPI_QUARTER_SPEED)) {
     Serial.println("initialization failed!");
@@ -164,47 +98,32 @@ void setup() {
   }
   Serial.println("initialization done.");
 
-  Serial.println();
-
-  char tmpStr[25];
-  strcat(tmpStr, "Temperature = ");
-  Serial.println(tmpStr);
-  char tmp[6];
-  Serial.println(bme.readTemperature());
-  strcat(tmpStr, dtostrf(bme.readTemperature(), 6, 2, tmp));
-  strcat(tmpStr, " *C");
-  Serial.println(tmpStr);
-
-  char presStr[25];
-  strcat(presStr, "Pressure = ");
-  char pres[9];
-  Serial.println(bme.readPressure());
-  strcat(presStr, dtostrf(bme.readPressure(), 9, 2, pres));
-  strcat(presStr, " Pa");
-  Serial.println(presStr);
-
-  char altStr[25];
-  strcat(altStr, "Altitude = ");
-  char alt[9];
-  Serial.println(bme.readPressure());
-  strcat(altStr, dtostrf(bme.readPressure(), 9, 2, alt));
-  strcat(altStr, " m");
-  Serial.println(altStr);
-
-  char tmpFile[] = "temptest.txt";
-  char presFile[] = "pressureTest.txt";
-  char altFile[] = "altTest.txt";
-  
-  writeToFile(tmpFile, tmpStr); // Temperature
-  writeToFile(presFile, presStr);  // Pressure
-  writeToFile(altFile, altStr); // Altitude (approximated)
-
-  readFile(tmpFile);
-  readFile(presFile);
-  readFile(altFile);
+  // Initialize bmp
+  if (!bme.begin()) {  
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+    while (1);
+  } else {
+    Serial.println("BMP280 sensor detected.");
+  }
 }
 
 void loop() {
   // nothing happens after setup
+
+  char tmpFile[] = "temptest.txt";
+  char presFile[] = "presTest.txt";
+  char altFile[] = "altTest.txt";
+
+  char what[] = "temp";
+  float temp = readBMP(what);
+  strcpy(what, "pres");
+  float pres = readBMP(what);
+  strcpy(what, "alti");
+  float alt = readBMP(what);
   
+  writeToFile(tmpFile, temp); // Temperature
+  writeToFile(presFile, pres);  // Pressure
+  writeToFile(altFile, alt); // Altitude (approximated)
+
+  delay(2000);
 }
