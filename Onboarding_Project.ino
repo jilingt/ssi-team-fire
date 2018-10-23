fe1c6b34aea6349c2eddf17d5c3614ad5e6acb6b
jilingt <44306377+jilingt@users.noreply.github.com>
2018-10-23 02:18:20 -0700

    Added GPS
    GPS data currently not very formatted though

--- Onboarding_Project.ino
+++ Onboarding_Project.ino
@@ -1,6 +1,6 @@
 /* Onboarding Project - Team Fire
  * 
- * Updated to initialize and de-initialize each sensor
+ * Updated to change sensor wiring to I2C and add GPS
  */
 
 #include <Wire.h>
@@ -13,8 +13,18 @@ SdFat sd;
 SdFile file;
 File myFile;
 
+#include <SoftwareSerial.h>
+
+#include <TinyGPS.h>
+
+TinyGPS gps;
+
+// Use one of these to connect your GPS
+// ------------------------------------
+#define gpsPort Serial1
+
 const int SD_CS = 9;
-const int BMP_CS = 10;
+//const int BMP_CS = 10;
 const double altCalibrate = 1014.5;
 
 int times = 0;
@@ -28,16 +38,36 @@ void writeToFile(char filename[], float writeLine) {
 
   // if the file opened okay, write to it:
   if (myFile) {
-    Serial.print("Writing to ");
-    Serial.print(filename);
+//    Serial.print("Writing to ");
+//    Serial.print(filename);
     myFile.println(writeLine);
     // close the file:
     myFile.close();
-    Serial.println(". Done.");
+//    Serial.println(". Done.");
   } else {
     // if the file didn't open, print an error:
-    Serial.print("error opening ");
-    Serial.println(filename);
+//    Serial.print("error opening ");
+//    Serial.println(filename);
+  }
+}
+
+void writeToFile(char filename[], char writeLine[]) {
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  myFile = sd.open(filename, FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+//    Serial.print("Writing to ");
+//    Serial.print(filename);
+    myFile.println(writeLine);
+    // close the file:
+    myFile.close();
+//    Serial.println(". Done.");
+  } else {
+    // if the file didn't open, print an error:
+//    Serial.print("error opening ");
+//    Serial.println(filename);
   }
 }
 
@@ -71,7 +101,7 @@ float readBMP(char readWhat[]) {
   } else if(strcmp(readWhat, "alti") == 0) {
     return bme.readAltitude();
   } else {
-    Serial.println("parameter not found!");
+//    Serial.println("parameter not found!");
   }
 
   return 0;
@@ -88,31 +118,41 @@ void setup() {
 
   SPI.setMOSI(11);
   SPI.setMISO(12);
-  SPI.setSCK(13);
+  SPI.setSCK(14);
   
   // Initialize SD card
-  Serial.print("Initializing SD card...");
+//  Serial.print("Initializing SD card...");
   if (!sd.begin(SD_CS, SPI_QUARTER_SPEED)) {
-    Serial.println("initialization failed!");
+//    Serial.println("initialization failed!");
     return;
   }
-  Serial.println("initialization done.");
+//  Serial.println("initialization done.");
 
   // Initialize bmp
   if (!bme.begin()) {  
-    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+//    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
     while (1);
   } else {
-    Serial.println("BMP280 sensor detected.");
+//    Serial.println("BMP280 sensor detected.");
   }
+
+  // Starts GPS mwahahaha
+  gpsPort.begin(4800);
 }
 
 void loop() {
+  // GPS nonsense
+  float flat, flon;
+  unsigned long age, chars = 0;
+  unsigned short sentences = 0, failed = 0;
+  static const double LONDON_LAT = 51.508131, LONDON_LON = -0.128002;
+  
   // nothing happens after setup
 
   char tmpFile[] = "temptest.txt";
   char presFile[] = "presTest.txt";
-  char altFile[] = "altTest.txt";
+  char altFile[] = "BMPalt.txt";
+  char gpsFile[] = "gpsTest.txt";
 
   char what[] = "temp";
   float temp = readBMP(what);
@@ -120,10 +160,35 @@ void loop() {
   float pres = readBMP(what);
   strcpy(what, "alti");
   float alt = readBMP(what);
+
+  gps.f_get_position(&flat, &flon, &age);
+  int year;
+  byte month, day, hour, minute, second, hundredths;
+  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
+  char sz[32];
+  sprintf(sz, "%02d/%02d/%02d %02d:%02d:%02d ",
+          month, day, year, hour, minute, second);
   
   writeToFile(tmpFile, temp); // Temperature
+//  Serial.print("Temperature: ");
+//  Serial.println(temp);
   writeToFile(presFile, pres);  // Pressure
+//  Serial.print("Pressure: ");
+//  Serial.println(pres);
   writeToFile(altFile, alt); // Altitude (approximated)
-
+//  Serial.print("BMP Altitude: ");
+//  Serial.println(alt);
+  writeToFile(gpsFile, flat);
+//  Serial.print("Flat: ");
+//  Serial.println(flat);
+  writeToFile(gpsFile, flon);
+//  Serial.print("Flon: ");
+//  Serial.println(flon);
+  writeToFile(gpsFile, gps.f_altitude());
+//  Serial.print("GPS Altitude:");
+//  Serial.println(gps.f_altitude());
+  writeToFile(gpsFile, sz);
+//  Serial.print("Date and time: ");
+//  Serial.println(sz);
   delay(2000);
 }
