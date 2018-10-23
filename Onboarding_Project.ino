640bed3e1b306d52a431dac3e2bb17dc8dc8d117
jilingt <44306377+jilingt@users.noreply.github.com>
2018-10-22 22:05:30 -0700

    Corrections to initialize and un-initialize
    Does not actually work

--- Onboarding_Project.ino
+++ Onboarding_Project.ino
@@ -15,11 +15,13 @@ File myFile;
 
 const int SD_CS = 9;
 const int BMP_CS = 10;
-const double altCalibrate = 1013.25;
+const double altCalibrate = 1014.5;
+
+int times = 0;
 
 Adafruit_BMP280 bme(BMP_CS);
 
-void writeToFile(char filename[], char writeLine[]) {
+void writeToFile(char filename[], float writeLine) {
   SPI.setMOSI(11);
   SPI.setMISO(12);
   SPI.setSCK(13);
@@ -51,43 +53,6 @@ void writeToFile(char filename[], char writeLine[]) {
   }
 
   // Closes the SD card (? hopefully?)
-  root.close();
-  SPI.endTransaction();
-}
-
-void readBMP(char &tmpStr[], char &presStr[], char &altStr[]) {
-  SPI.setMOSI(11);
-  SPI.setMISO(12);
-  SPI.setSCK(13);
-  
-  // Initialize bmp
-  if (!bme.begin()) {  
-    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
-    while (1);
-  } else {
-    Serial.println("BMP280 sensor detected.");
-  }
-
-  strcat(tmpStr, "Temperature = ");
-  Serial.println(tmpStr);
-  char tmp[6];
-  Serial.println(bme.readTemperature());
-  strcat(tmpStr, dtostrf(bme.readTemperature(), 6, 2, tmp));
-  strcat(tmpStr, " *C");
-  
-  strcat(presStr, "Pressure = ");
-  char pres[9];
-  Serial.println(bme.readPressure());
-  strcat(presStr, dtostrf(bme.readPressure(), 9, 2, pres));
-  strcat(presStr, " Pa");
-  
-  strcat(altStr, "Altitude = ");
-  char alt[9];
-  Serial.println(bme.readPressure());
-  strcat(altStr, dtostrf(bme.readPressure(), 9, 2, alt));
-  strcat(altStr, " m");
-
-  // Hopefully this ends the BMP
   SPI.endTransaction();
 }
 
@@ -125,40 +90,75 @@ void readFile(char filename[]) {
   }
 
   // Closes the SD card (? hopefully?)
-  root.close();
   SPI.endTransaction();
 }
 
-void setup() {
+float readBMP(char readWhat[]) {
+  SPI.setMOSI(11);
+  SPI.setMISO(12);
+  SPI.setSCK(13);
+//
+//  digitalWrite(SD_CS, LOW);
+//  digitalWrite(BMP_CS, HIGH);
+//  pinMode(BMP_CS, OUTPUT);
   
+  // Initialize bmp
+  if (!bme.begin()) {  
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+    while (1);
+  } else {
+    Serial.println("BMP280 sensor detected.");
+  }
+  
+  if(strcmp(readWhat, "temp") == 0) {
+    return bme.readTemperature();
+  } else if(strcmp(readWhat, "pres") == 0) {
+    return bme.readPressure();
+  } else if(strcmp(readWhat, "alti") == 0) {
+    return bme.readAltitude();
+  } else {
+    Serial.println("parameter not found!");
+  }
+
+  SPI.endTransaction();
+
+  return 0;
+}
+
+void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-
-  char tmpStr[25];
-  char presStr[25];
-  char altStr[25];
-
-  Serial.println();
-
-  readBMP(tmpStr, presStr, altStr);
-
-  char tmpFile[] = "temptest.txt";
-  char presFile[] = "pressureTest.txt";
-  char altFile[] = "altTest.txt";
-  
-  writeToFile(tmpFile, tmpStr); // Temperature
-  writeToFile(presFile, presStr);  // Pressure
-  writeToFile(altFile, altStr); // Altitude (approximated)
-
-  readFile(tmpFile);
-  readFile(presFile);
-  readFile(altFile);
 }
 
 void loop() {
   // nothing happens after setup
+
+  char tmpFile[] = "temptest.txt";
+  char presFile[] = "presTest.txt";
+  char altFile[] = "altTest.txt";
+
+  if(times < 100) {
+    char what[] = "temp";
+    float temp = readBMP(what);
+    strcpy(what, "pres");
+    float pres = readBMP(what);
+    strcpy(what, "alti");
+    float alt = readBMP(what);
+  
+    writeToFile(tmpFile, temp); // Temperature
+    writeToFile(presFile, pres);  // Pressure
+    writeToFile(altFile, alt); // Altitude (approximated)
+
+    Serial.println();
+
+    times++;
+  } else if(times == 100) {
+    readFile(tmpFile);
+    readFile(presFile);
+    readFile(altFile);
+  }
   
 }
