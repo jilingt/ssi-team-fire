bcbbbea65cd7b4522d0a685ac713bc4c9b43ead8
jilingt <44306377+jilingt@users.noreply.github.com>
2018-10-25 01:31:36 -0700

    Not working, GPS and Iridium added

--- Onboarding_Project.ino
+++ Onboarding_Project.ino
@@ -3,13 +3,13 @@
  * Updated to change sensor wiring to I2C and add GPS
  * Cleaned
  */
-
 #include <Wire.h>
 #include <SPI.h>
 #include "SdFat.h"
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
 #include <TinyGPS.h>
+#include <IridiumSBD.h>
 
 // SD Card
 SdFat sd;
@@ -17,23 +17,31 @@ SdFile file;
 File myFile;
 
 // GPS
-#define gpsPort Serial1
 TinyGPS gps;
+#define gpsPort Serial1
+
+#define IridiumSerial Serial3
+
+// Declare the IridiumSBD object
+IridiumSBD modem(IridiumSerial);
 
 // BMP
 Adafruit_BMP280 bme;
 
 const int SD_CS = 9; // Chip select for SD
-const double altCalibrate = 1014.5; // Change this to current pressure at sea level
 
-void writeToFile(char filename[], float writeLine) {
+
+int seconds;
+
+void writeToFile(char filename[], float writeLine, int prec) {
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
   myFile = sd.open(filename, FILE_WRITE);
 
   // if the file opened okay, write to it:
   if (myFile) {
-    myFile.println(writeLine);
+    Serial.println(writeLine, prec);
+    myFile.println(writeLine, prec);
     myFile.close();
   }
 }
@@ -45,42 +53,21 @@ void writeToFile(char filename[], char writeLine[]) {
 
   // if the file opened okay, write to it:
   if (myFile) {
+    Serial.println(writeLine);
     myFile.println(writeLine);
     // close the file:
     myFile.close();
   }
 }
 
-// NOTE: this function probably won't be used (since we can't output to serial)
-void readFile(char filename[]) {
-  // open the file for reading:
-  myFile = sd.open(filename);
-  if (myFile) {
-    Serial.print(filename);
-    Serial.println(":");
-
-    // read from the file until there's nothing else in it:
-    while (myFile.available()) {
-      Serial.write(myFile.read());
-    }
-    // close the file:
-    myFile.close();
-
-    Serial.println();
-  } else {
-    // if the file didn't open, print an error:
-    Serial.print("error opening ");
-    Serial.println(filename);
-  }
-}
-
 void setup() {
   // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+  Serial.begin(115200);
 
+  // Initialize GPS port
+  Serial1.begin(9600);
+
+  // Initialize SD port
   SPI.setMOSI(11);
   SPI.setMISO(12);
   SPI.setSCK(14);
@@ -91,60 +78,105 @@ void setup() {
   // Initialize bmp
   bme.begin();
 
-  // Starts GPS mwahahaha
-  gpsPort.begin(4800);
+  // Initialize RockBlock
+  IridiumSerial.begin(19200);
+  seconds = 0;
+
 }
 
 void loop() {
-  // GPS nonsense
-  float flat, flon;
-  unsigned long age, chars = 0;
-  unsigned short sentences = 0, failed = 0;
-  static const double LONDON_LAT = 51.508131, LONDON_LON = -0.128002;
-  int year;
-  byte month, day, hour, minute, second, hundredths;
-  char sz[32];
-
-  // Filenames (remember can't be longer than 8 chars for FAT)
-  char tmpFile[] = "temptest.txt";
-  char presFile[] = "presTest.txt";
-  char altFile[] = "BMPalt.txt";
-  char gpsFile[] = "gpsTest.txt";
-
+  
+  char master[] = "master.txt";
+  
   // Getting data from BMP
   float temp = bme.readTemperature();
   float pres = bme.readPressure();
   float alt = bme.readAltitude();
 
   // Getting data from GPS
+  float flat, flon;
+  unsigned long age = 0;
   gps.f_get_position(&flat, &flon, &age);
+  writeToFile(master, "****");
+  print_date(gps, master);
+
+  writeToFile(master, temp, 2); // Temperature
+  writeToFile(master, pres, 2);  // Pressure
+  writeToFile(master, alt, 2); // BMP Altitude (approximated)
+  
+  writeToFile(master, flat, 10);
+  writeToFile(master, flon, 10);
+  writeToFile(master, gps.f_altitude(), 2);
+  
+
+  // Vital method; do not remove!
+  smartdelay(1000);
+
+
+  char buff[20];
+  String toSend = "";
+  
+  dtostrf(alt, 4, 2, buff);
+  toSend += buff;
+  toSend += ",";
+  dtostrf(flat, 4, 10, buff);
+  toSend += buff;
+  toSend += ",";
+  dtostrf(flon, 4, 10, buff);
+  toSend+= buff;
+
+  int signalQuality;
+  int err = modem.getSignalQuality(signalQuality);
+  if (err == ISBD_SUCCESS)
+  {
+    if (signalQuality > 0 && signalQuality > (150 - seconds) / 30) 
+    {
+      err = modem.sendSBDText("1000,37,-122");
+      if (err != ISBD_SUCCESS) {
+        writeToFile(master, "Failure: poor signal");
+      }
+      else {
+        writeToFile(master, "Hey, it worked!");
+        seconds = 0;
+      }
+    }
+  }
+  else {
+    writeToFile(master, "Failure: can't get signal quality");  
+  }
+  
+
+  delay(20000);
+  
+  seconds += 20;
+}
+
+
+static void smartdelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do 
+  {
+    while (Serial1.available())
+      gps.encode(Serial1.read());
+  } while (millis() - start < ms);
+}
+
+
+static void print_date(TinyGPS &gps, char filename[])
+{
+  int year;
+  byte month, day, hour, minute, second, hundredths;
+  unsigned long age;
   gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
-  sprintf(sz, "%02d/%02d/%02d %02d:%02d:%02d ", month, day, year, hour, minute, second);
-
-  // Writing data to files
-  writeToFile(tmpFile, temp); // Temperature
-  writeToFile(presFile, pres);  // Pressure
-  writeToFile(altFile, alt); // BMP Altitude (approximated)
-  writeToFile(gpsFile, flat); // Latitude
-  writeToFile(gpsFile, flon); // Longitude
-  writeToFile(gpsFile, gps.f_altitude()); // GPS Altitude
-  writeToFile(gpsFile, sz); // GPS date and time
-
-//  Print statements for all variables written to files
-//  Serial.print("Temperature: ");
-//  Serial.println(temp);
-//  Serial.print("Pressure: ");
-//  Serial.println(pres);
-//  Serial.print("BMP Altitude: ");
-//  Serial.println(alt);
-//  Serial.print("Flat: ");
-//  Serial.println(flat);
-//  Serial.print("Flon: ");
-//  Serial.println(flon);
-//  Serial.print("GPS Altitude:");
-//  Serial.println(gps.f_altitude());
-//  Serial.print("Date and time: ");
-//  Serial.println(sz);
-
-  delay(2000);
+  if (age == TinyGPS::GPS_INVALID_AGE)
+    writeToFile(master, "GPS NO SIGNAL");
+  else
+  {
+    char sz[32];
+    sprintf(sz, "%02d/%02d/%02d %02d:%02d:%02d ",
+        month, day, year, hour, minute, second);
+    writeToFile(filename, sz);
+  }
+  smartdelay(0);
 }
