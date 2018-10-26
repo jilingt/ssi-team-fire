1c95124470ce4aa9217fc927d33ab8101efc5a68
jilingt <44306377+jilingt@users.noreply.github.com>
2018-10-26 01:41:10 -0700

    Rockblock transmission success!
    Successfully transmits BMP altitude and GPS data

--- Onboarding_Project.ino
+++ Onboarding_Project.ino
@@ -1,7 +1,8 @@
 /* Onboarding Project - Team Fire
  * 
- * Updated to change sensor wiring to I2C and add GPS
- * Cleaned
+ * Updates
+ * *Successfully transmits via RockBlock
+ * *Transmits BMP altitude and GPS data
  */
 #include <Wire.h>
 #include <SPI.h>
@@ -20,9 +21,8 @@ File myFile;
 TinyGPS gps;
 #define gpsPort Serial1
 
+// Iridium SBD
 #define IridiumSerial Serial3
-
-// Declare the IridiumSBD object
 IridiumSBD modem(IridiumSerial);
 
 // BMP
@@ -30,8 +30,7 @@ Adafruit_BMP280 bme;
 
 const int SD_CS = 9; // Chip select for SD
 
-
-int seconds;
+int seconds = 180;  // Seconds since last transmission; initialized to send upon first connection
 
 void writeToFile(char filename[], float writeLine, int prec) {
   // open the file. note that only one file can be open at a time,
@@ -60,7 +59,49 @@ void writeToFile(char filename[], char writeLine[]) {
   }
 }
 
+void writeToFile(char filename[], int writeLine) {
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  myFile = sd.open(filename, FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.println(writeLine);
+    myFile.println(writeLine);
+    // close the file:
+    myFile.close();
+  }
+}
+
+static void smartdelay(unsigned long ms) {
+  unsigned long start = millis();
+  do {
+    while (Serial1.available()) {
+      gps.encode(Serial1.read());
+    }
+  } while (millis() - start < ms);
+}
+
+static void print_date(TinyGPS &gps, char filename[]) {
+  int year;
+  byte month, day, hour, minute, second, hundredths;
+  unsigned long age;
+  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
+  if (age == TinyGPS::GPS_INVALID_AGE) {
+    char tmp[] = "GPS NO SIGNAL";
+    writeToFile(filename, tmp);
+  } else {
+    char sz[32] = "";
+    sprintf(sz, "%02d/%02d/%02d %02d:%02d:%02d ",
+        month, day, year, hour, minute, second);
+    writeToFile(filename, sz);
+  }
+  smartdelay(0);
+}
+
 void setup() {
+  char master[] = "master.txt"; // filename
+  
   // Open serial communications and wait for port to open:
   Serial.begin(115200);
 
@@ -80,13 +121,26 @@ void setup() {
 
   // Initialize RockBlock
   IridiumSerial.begin(19200);
-  seconds = 0;
 
+  // Initialize RockBlock modem
+  int err;
+  err = modem.begin();
+  if (err != ISBD_SUCCESS) {
+    char tmp[] = "Begin failed: error";
+    writeToFile(master, tmp);
+    writeToFile(master, err);
+    if (err == ISBD_NO_MODEM_DETECTED) {
+      strcpy(tmp, "No modem detected: check wiring.");
+      writeToFile(master, tmp);
+    }
+  } else {
+    char tmp[] = "Modem initialized.";
+    writeToFile(master, tmp);
+  }
 }
 
 void loop() {
-  
-  char master[] = "master.txt";
+  char master[] = "master.txt"; // Filename
   
   // Getting data from BMP
   float temp = bme.readTemperature();
@@ -97,86 +151,64 @@ void loop() {
   float flat, flon;
   unsigned long age = 0;
   gps.f_get_position(&flat, &flon, &age);
-  writeToFile(master, "****");
+  char tmp[] = "****";
+  writeToFile(master, tmp);
   print_date(gps, master);
 
   writeToFile(master, temp, 2); // Temperature
   writeToFile(master, pres, 2);  // Pressure
   writeToFile(master, alt, 2); // BMP Altitude (approximated)
   
-  writeToFile(master, flat, 10);
-  writeToFile(master, flon, 10);
-  writeToFile(master, gps.f_altitude(), 2);
-  
+  writeToFile(master, flat, 10);  // GPS Latitude
+  writeToFile(master, flon, 10);  // GPS Longitude
+  writeToFile(master, gps.f_altitude(), 2); // GPS Altitude
 
-  // Vital method; do not remove!
+  // Vital method for GPS; do not remove!
   smartdelay(1000);
 
+  // Data to be sent
+  char buff[20] = "";
+  char toSend[42] = "";
+  dtostrf(alt, 9, 2, buff);
+  strcat(toSend, buff);
+  strcat(toSend, ",");
+  dtostrf(flat, 15, 10, buff);
+  strcat(toSend, buff);
+  strcat(toSend, ",");
+  dtostrf(flon, 15, 10, buff);
+  strcat(toSend, buff);
 
-  char buff[20];
-  String toSend = "";
-  
-  dtostrf(alt, 4, 2, buff);
-  toSend += buff;
-  toSend += ",";
-  dtostrf(flat, 4, 10, buff);
-  toSend += buff;
-  toSend += ",";
-  dtostrf(flon, 4, 10, buff);
-  toSend+= buff;
+  Serial.print("data to send: ");
+  Serial.println(toSend);
 
   int signalQuality;
   int err = modem.getSignalQuality(signalQuality);
-  if (err == ISBD_SUCCESS)
-  {
-    if (signalQuality > 0 && signalQuality > (150 - seconds) / 30) 
-    {
-      err = modem.sendSBDText("1000,37,-122");
+  if (err == ISBD_SUCCESS) {
+    Serial.print("Signal quality: ");
+    Serial.println(signalQuality);
+    Serial.print("seconds: ");
+    Serial.println(seconds);
+    if ((signalQuality > 2 || (signalQuality > 0 && seconds >= 300)) && seconds >= 180) {
+      Serial.println("Trying to send.");
+      err = modem.sendSBDText(toSend);
       if (err != ISBD_SUCCESS) {
-        writeToFile(master, "Failure: poor signal");
-      }
-      else {
-        writeToFile(master, "Hey, it worked!");
+        char tmp[] = "Failure to send: poor signal";
+        writeToFile(master, tmp);
+      } else {
+        char tmp[] = "Hey, it worked!";
+        writeToFile(master, tmp);
         seconds = 0;
       }
+    } else {
+      char tmp[] = "signalQuality not greater than 0; max time not yet exceeded";
+      writeToFile(master, tmp);
     }
-  }
-  else {
-    writeToFile(master, "Failure: can't get signal quality");  
+  } else {
+    char tmp[] = "Failure: can't get signal quality";
+    writeToFile(master, tmp);  
   }
   
-
   delay(20000);
   
   seconds += 20;
-}
-
-
-static void smartdelay(unsigned long ms)
-{
-  unsigned long start = millis();
-  do 
-  {
-    while (Serial1.available())
-      gps.encode(Serial1.read());
-  } while (millis() - start < ms);
-}
-
-
-static void print_date(TinyGPS &gps, char filename[])
-{
-  int year;
-  byte month, day, hour, minute, second, hundredths;
-  unsigned long age;
-  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
-  if (age == TinyGPS::GPS_INVALID_AGE)
-    writeToFile(master, "GPS NO SIGNAL");
-  else
-  {
-    char sz[32];
-    sprintf(sz, "%02d/%02d/%02d %02d:%02d:%02d ",
-        month, day, year, hour, minute, second);
-    writeToFile(filename, sz);
-  }
-  smartdelay(0);
 }
